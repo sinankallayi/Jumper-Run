cb9da2ffaa9f40b5b01582df3689eb0cd50ea8bf
Sinan <sinukallayi@gmail.com>
2021-03-24 21:28:22 +0530

    add character

--- game.js
+++ game.js
@@ -1,26 +1,27 @@
-var character=document.getElementById("character");
-var block=document.getElementById("block");
-var counter=0;
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter = 0;
 
-function jump(){
-    if(character.classList=="animate"){
+function jump() {
+    if (character.classList == "animate") {
         return;
     }
     character.classList.add("animate");
-    setTimeout(function(){character.classList.remove("animate");},300);
+    setTimeout(function () { character.classList.remove("animate"); }, 300);
 }
 
-var checkDead=setInterval(function(){
-    let characterTop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));   
-    let blockLeft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+var checkDead = setInterval(function () {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(blockLeft <20 && blockLeft>-20 && characterTop >= 130){
-block.style.animation="none";
-alert("You Dead.\n\nYour Score:" + Math.floor(counter/100));
-counter=0;
-block.style.animation="block 1s infinite linear";
-} else{
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+        block.style.animation = "none";
+        /*alert("You Dead.\n\nYour Score:" + Math.floor(counter/100)); */
+        alert();    
+        counter = 0;
+        block.style.animation = "block 1s infinite linear";
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-}
-},10);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+    }
+}, 10);
